ddb1bae49be54231bc366939a9ef447b842d7960
Siddharth Jain <siddharthj1405@gmail.com>
2024-01-27 20:16:22 +0530

    Optimise JavaScript

--- index.js
+++ index.js
@@ -1,65 +1,62 @@
-let O = true;
-let X = false;
-let array = [];
+let result = document.querySelector(".result");
+let buttons = document.querySelectorAll(".btn");
+let playerXScore = document.querySelector(".playerXScore");
+let playerOScore = document.querySelector(".playerOScore");
+const win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const PLAYER_O = "O";
+const PLAYER_X = "X";
+let currentPlayer = PLAYER_O;
 let n = 0;
-document.querySelectorAll(".btn").forEach((choice) => {
+
+buttons.forEach((choice) => {
   choice.addEventListener("click", () => {
     n++;
-    if (O) {
-      choice.innerHTML = "O";
-      array[choice.id] = "O";
-      O = false;
-      X = true;
-    } else {
-      choice.innerHTML = "X";
-      array[choice.id] = "X";
-      X = false;
-      O = true;
-    }
+    choice.innerHTML = currentPlayer;
+    currentPlayer = currentPlayer === PLAYER_O ? PLAYER_X : PLAYER_O;
+    choice.disabled = true;
     if (checkWin("X")) {
-      document.querySelector(".playerXScore").innerHTML++;
-      document.querySelector(".result").innerHTML = "X Won!";
-      reset();
+      playerXScore.innerHTML++;
+      result.innerHTML = "X Won!";
+      setTimeout(reset, 1000);
     } else if (checkWin("O")) {
-      document.querySelector(".playerOScore").innerHTML++;
-      document.querySelector(".result").innerHTML = "O Won!";
-
-      reset();
+      playerOScore.innerHTML++;
+      result.innerHTML = "O Won!";
+      setTimeout(reset, 1000);
     } else if (n == 9) {
-      document.querySelector(".result").innerHTML = "It's a Draw!";
-      reset();
+      result.innerHTML = "It's a Draw!";
+      setTimeout(reset, 1000);
     }
   });
 });
 function checkWin(char) {
-  if (
-    valuesEqual(0, 1, 2, char) ||
-    valuesEqual(3, 4, 5, char) ||
-    valuesEqual(6, 7, 8, char) ||
-    valuesEqual(0, 3, 6, char) ||
-    valuesEqual(1, 4, 7, char) ||
-    valuesEqual(2, 5, 8, char) ||
-    valuesEqual(0, 4, 8, char) ||
-    valuesEqual(2, 4, 6, char)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
+  let hasWin = false;
+  win.forEach((element) => {
+    let val1 = buttons[element[0]].innerHTML;
+    let val2 = buttons[element[1]].innerHTML;
+    let val3 = buttons[element[2]].innerHTML;
 
-function valuesEqual(a, b, c, char) {
-  if (array[a] == char && array[b] == char && array[c] == char) {
-    return true;
-  } else {
-    return false;
-  }
+    if (val1 != "" && val2 != "" && val3 != "") {
+      if (val1 == val2 && val2 == val3 && val3 == char) {
+        hasWin = true;
+      }
+    }
+  });
+  return hasWin;
 }
 
 function reset() {
-  array = [];
   n = 0;
-  document.querySelectorAll(".btn").forEach((choice) => {
+  buttons.forEach((choice) => {
     choice.innerHTML = "";
+    choice.disabled = false;
   });
 }
