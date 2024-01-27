a7f71492562a5cd2d6f6a36b96ecbb633194ba68
Siddharth Jain <siddharthj1405@gmail.com>
2024-01-27 23:01:06 +0530

    Better UI and UX

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let result = document.querySelector(".result");
-let buttons = document.querySelectorAll(".btn");
-let playerXScore = document.querySelector(".playerXScore");
-let playerOScore = document.querySelector(".playerOScore");
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".btn");
+const playerXScore = document.querySelector(".playerXScore");
+const playerOScore = document.querySelector(".playerOScore");
 const win = [
   [0, 1, 2],
   [3, 4, 5],
@@ -21,36 +21,51 @@ buttons.forEach((choice) => {
   choice.addEventListener("click", () => {
     n++;
     choice.innerHTML = currentPlayer;
-    currentPlayer = currentPlayer === PLAYER_O ? PLAYER_X : PLAYER_O;
     choice.disabled = true;
-    if (checkWin("X")) {
+    if (currentPlayer === PLAYER_O) {
+      currentPlayer = PLAYER_X;
+      choice.style.color = "blue";
+      
+    } else {
+      currentPlayer = PLAYER_O;
+      choice.style.color = "red";
+    }
+    let ifWin = checkWin();
+    if (ifWin == "X") {
       playerXScore.innerHTML++;
-      result.innerHTML = "X Won!";
+      result.innerHTML = "X Won🏆";
+      result.style.color = "red";
       setTimeout(reset, 1000);
-    } else if (checkWin("O")) {
+    } else if (ifWin == "O") {
       playerOScore.innerHTML++;
-      result.innerHTML = "O Won!";
+      result.innerHTML = "O Won🏆";
+      result.style.color = "blue";
       setTimeout(reset, 1000);
     } else if (n == 9) {
       result.innerHTML = "It's a Draw!";
+      result.style.color = "yellow";
       setTimeout(reset, 1000);
     }
   });
 });
-function checkWin(char) {
-  let hasWin = false;
+
+function checkWin() {
+  let winner = false;
   win.forEach((element) => {
     let val1 = buttons[element[0]].innerHTML;
     let val2 = buttons[element[1]].innerHTML;
     let val3 = buttons[element[2]].innerHTML;
-
-    if (val1 != "" && val2 != "" && val3 != "") {
-      if (val1 == val2 && val2 == val3 && val3 == char) {
-        hasWin = true;
-      }
+    if (
+      val1 != "" &&
+      val2 != "" &&
+      val3 != "" &&
+      val1 == val2 &&
+      val2 == val3
+    ) {
+      winner = val1;
     }
   });
-  return hasWin;
+  return winner;
 }
 
 function reset() {
@@ -58,5 +73,7 @@ function reset() {
   buttons.forEach((choice) => {
     choice.innerHTML = "";
     choice.disabled = false;
+    result.innerHTML = "Pick your move!";
+    result.style.color = "#12e049";
   });
 }
